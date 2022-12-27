882604c243a8bf0f3eddd06fc8c6894a14c4d04e
Huseyn Abbasov <nelbekistekan12@gmail.com>
2022-12-27 14:50:25 +0400

    Add project files.

---
+++ TransferMoneyHomeWork/App.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace TransferMoneyHomeWork
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+        private static Mutex _mutex = null;
+        public static bool IsProgressRun = false;
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            
+            const string appName = "MyAppName";
+            bool createdNew;
+            
+            _mutex = new Mutex(true, appName, out createdNew);
+
+            //For test
+
+            //if (!createdNew)
+            //{
+            //    //app is already running! Exiting the application  
+            //    MessageBox.Show("App is already running");
+            //    Application.Current.Shutdown();
+            //}
+
+            base.OnStartup(e);
+        }
+
+    }
+}
---
+++ TransferMoneyHomeWork/ViewModel/MainViewModel.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Threading;
+using TransferMoneyHomeWork.RelayCommands;
+
+namespace TransferMoneyHomeWork.ViewModel
+{
+    public class MainViewModel:BaseViewModel
+    {
+
+        public RelayCommand CloseCommand { get; set; }
+        public RelayCommand InsertCardCommand { get; set; }
+        public RelayCommand LoadDataCommand { get; set; }
+        public RelayCommand TransferMoneyCommand { get; set; }
+        public RelayCommand ShowTest { get; set; }
+        
+
+        public string CardId { get; set; }
+        //User 
+        public int BalanceInt { get; set; }
+        private string username;
+
+        public string Username
+        {
+            get { return username; }
+            set { username = value;OnPropertyChanged(); }
+        }
+
+        private string balance;
+
+        public string Balance
+        {
+            get { return balance; }
+            set { balance = value;OnPropertyChanged(); }
+        }
+
+
+        private string flagEnabled;
+
+        public string FlagEnabled
+        {
+            get { return flagEnabled; }
+            set { flagEnabled = value;OnPropertyChanged(); }
+        }
+
+        private string message;
+
+        public string Message
+        {
+            get { return message; }
+            set { message = value;OnPropertyChanged(); }
+        }
+
+
+        public MainWindow mainWindow { get; set; }
+
+
+        public DispatcherTimer dispatcherTimer { get; set; }
+        public bool ProgramCanClose { get; set; } = true;
+
+        public static Mutex mutex { get; set; }
+        string mutexName = "TestMutex";
+
+        public MainViewModel(MainWindow window)
+        {
+            dispatcherTimer = new DispatcherTimer();
+            //This Is For Trigger Logic For Transfer Money
+            dispatcherTimer.Tick += TransferMoneyFunc;
+            dispatcherTimer.Interval = TimeSpan.FromSeconds(1);
+
+            FlagEnabled = "False";
+            mainWindow = window;
+
+            mutex = new Mutex(true,mutexName);
+
+
+            CloseCommand = new RelayCommand((o) =>
+            {
+                if(ProgramCanClose==true)
+                mainWindow.Close();
+            });
+
+            InsertCardCommand = new RelayCommand((o) =>
+            {
+                FlagEnabled = "True";
+            });
+            LoadDataCommand = new RelayCommand((o) =>
+            {
+                
+                //This Is For Only Test
+                Username = "John";
+                Balance = "500";
+                BalanceInt = int.Parse(Balance);
+    
+            });
+            TransferMoneyCommand = new RelayCommand((o) =>
+            {
+                ProgramCanClose = false;
+                dispatcherTimer.Start();
+                App.IsProgressRun= true;
+            });
+
+            ShowTest = new RelayCommand((o) =>
+            {
+                
+            });
+        }
+
+        
+
+        //Logic For Transfer Money
+        private void TransferMoneyFunc(object sender, EventArgs e)
+        {
+            var balance2 = double.Parse(Balance);
+            if(balance2!=0)
+            {
+                balance2 -= (BalanceInt / 100) * 10;
+                Balance = balance2.ToString();
+            }
+            else
+            {
+                dispatcherTimer.Stop();
+                ProgramCanClose = true;
+                Message = "Transfer Completed Succesfully";
+            }
+        }
+        
+    }
+}
